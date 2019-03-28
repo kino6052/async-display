c837aea40e896effd3b77edc5831bc261f6fdce7
Kirill Novik <kino6052@colorado.edu>
2019-03-28 12:06:11 -0600

    Initial Commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const test = document.createElement("div");
+test.setAttribute("id", "test");
+test.innerText = "Test";
+
+let delay = 0;
+const delayMatch = window.location.search.match(/(?<=delay=)\d/g);
+if (delayMatch && delayMatch[0]) {
+    delay = Number(delayMatch[0]);
+}
+
+setTimeout(() => {
+    document.getElementById("root").appendChild(test)
+}, delay * 1000);
