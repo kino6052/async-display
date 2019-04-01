088320d91ac5d7ed39e1b725872f9e1686ad5df3
Kirill Novik <kino6052@colorado.edu>
2019-04-01 13:32:58 -0600

    Add async input

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const test = document.createElement("div");
 test.setAttribute("id", "test");
 test.innerText = "Test";
 
+const input = document.createElement("input");
+input.setAttribute("id", "input");
+
 let delay = 0;
 const delayMatch = window.location.search.match(/(?<=delay=)\d+/g);
 if (delayMatch && delayMatch[0]) {
@@ -9,5 +12,6 @@ if (delayMatch && delayMatch[0]) {
 }
 
 setTimeout(() => {
-    document.getElementById("root").appendChild(test)
+    document.getElementById("root").appendChild(test);
+    document.getElementById("root").appendChild(input);
 }, delay * 1000);
